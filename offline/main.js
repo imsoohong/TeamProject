d0aa80b3a86fbc860d9287f01b9bbbc6c6246039
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-03-22 19:29:58 +0900

    copy

---
+++ offline/main.js
@@ -0,0 +1,8 @@
+// const centerMenu = document.querySelector('.header__center');
+
+// const headerToggleBtn = document.querySelector('.header__toggle-btn');
+
+//   headerToggleBtn.addEventListener('click', () => {
+//   centerMenu.classList.toggle('open');
+//   console.log('ㅎㅇ');
+// });
